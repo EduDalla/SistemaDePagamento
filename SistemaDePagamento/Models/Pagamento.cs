e4971891d5b7b43a0f6ad66a6082362187b36c32
Eduardo Dallabella Lima <eduardodallabella@gmail.com>
2026-04-15 21:31:09 -0300

    Adiciona classe base de pagamento

---
+++ SistemaDePagamento/Models/Pagamento.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+
+namespace SistemaDePagamento.Models;
+
+public abstract class Pagamento
+{
+    public decimal Valor { get; set; }
+
+    public DateTime DataProcessamento { get; set; }
+
+    protected string FormatarValor()
+    {
+        return Valor.ToString("C", new CultureInfo("pt-BR"));
+    }
+
+    protected string FormatarData()
+    {
+        return DataProcessamento.ToString("dd/MM/yyyy");
+    }
+
+    public abstract string ProcessarPagamento();
+}
