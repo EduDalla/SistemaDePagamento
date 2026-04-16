911cd631a92561a686a14a107a0f7df6f2849358
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2026-04-16 14:28:24 -0300

    Merge branch 'main' of https://github.com/EduDalla/SistemaDePagamento

--- SistemaDePagamento/Models/Pagamento.cs
+++ SistemaDePagamento/Models/Pagamento.cs
@@ -7,12 +7,10 @@ public abstract class Pagamento
     public decimal Valor { get; set; }
 
     public DateTime DataProcessamento { get; set; }
-
     protected string FormatarValor()
     {
         return Valor.ToString("C", new CultureInfo("pt-BR"));
     }
-
     protected string FormatarData()
     {
         return DataProcessamento.ToString("dd/MM/yyyy");
