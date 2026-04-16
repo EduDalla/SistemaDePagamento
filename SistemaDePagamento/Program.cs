ac0b6aeef165d1ee0c6d2bb3db8f862db83c283c
Eduardo Dallabella Lima <eduardodallabella@gmail.com>
2026-04-15 21:35:13 -0300

    Adiciona processamento de pagamento com cartao

--- SistemaDePagamento/Program.cs
+++ SistemaDePagamento/Program.cs
@@ -8,13 +8,32 @@ while (true)
 
     switch (opcao)
     {
+        case "1":
+            ProcessarPagamentoCartao();
+            break;
         case "3":
             Console.WriteLine("Encerrando o sistema. Ate logo!");
             return;
         default:
-            Console.WriteLine("Opcao indisponivel no momento. Tente novamente.");
+            Console.WriteLine("Opcao invalida. Tente novamente.");
             break;
     }
 
     Console.WriteLine();
+}
+
+static void ProcessarPagamentoCartao()
+{
+    decimal valor = InputHelper.LerValorMonetario("Informe o valor do pagamento:");
+    string numeroCartao = InputHelper.LerTextoObrigatorio("Informe o numero do cartao:");
+
+    var pagamento = new PagamentoCartao
+    {
+        Valor = valor,
+        DataProcessamento = DateTime.Now,
+        NumeroCartao = numeroCartao
+    };
+
+    Console.WriteLine();
+    Console.WriteLine(pagamento.ProcessarPagamento());
 }
